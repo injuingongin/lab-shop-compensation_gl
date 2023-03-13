7a35042e1f870859262c9ebc4d32738ea44f2ba3
injuingongin <124846867+injuingongin@users.noreply.github.com>
2023-03-13 11:10:12 +0900

    commit

---
+++ delivery/src/main/java/labshopcompensationgl/domain/DeliveryStarted.java
@@ -0,0 +1,24 @@
+package labshopcompensationgl.domain;
+
+import labshopcompensationgl.domain.*;
+import labshopcompensationgl.infra.AbstractEvent;
+import java.util.*;
+import lombok.*;
+
+
+@Data
+@ToString
+public class DeliveryStarted extends AbstractEvent {
+
+    private Long id;
+    private String address;
+    private String customerId;
+    private Integer quantity;
+
+    public DeliveryStarted(Delivery aggregate){
+        super(aggregate);
+    }
+    public DeliveryStarted(){
+        super();
+    }
+}
